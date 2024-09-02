8372ce30d29d59f48ae2169eda2d1dbcb3de7e69
Yangdaehan <yangdh08@naver.com>
2024-09-02 15:54:05 +0900

    feat: 로그아웃 기능 구현

--- src/main/java/com/tta/geumgiri/controller/MemberApiController.java
+++ src/main/java/com/tta/geumgiri/controller/MemberApiController.java
@@ -2,7 +2,13 @@ package com.tta.geumgiri.controller;
 
 import com.tta.geumgiri.domain.dto.AddMemberRequest;
 import com.tta.geumgiri.service.MemberService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +22,13 @@ public class MemberApiController {
     @PostMapping("/login")
     public String signup(AddMemberRequest request){
         memberService.save(request);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response){
+        new SecurityContextLogoutHandler().logout(request, response,
+                SecurityContextHolder.getContext().getAuthentication());
         return "redirect:/login";
     }
 
