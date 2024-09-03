1edfa1944d729263166efed895ea48fba9e26e91
Yangdaehan <yangdh08@naver.com>
2024-09-03 16:49:59 +0900

    fix: 기존의 '/static/**'경로를 무시하는 방법에서 요청 권한을 설정하는 방식으로 변경

--- src/main/java/com/tta/geumgiri/config/WebSecurityConfig.java
+++ src/main/java/com/tta/geumgiri/config/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.tta.geumgiri.config;
 
-
 import com.tta.geumgiri.member.service.MemberDetailService;
 import com.tta.geumgiri.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
@@ -9,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,36 +19,27 @@ public class WebSecurityConfig {
 
     private final UserDetailsService userDetailsService;
 
-
-    // 스프링 시큐리티 기능 비활성화
-    @Bean
-    public WebSecurityCustomizer configure(){
-        return (web) -> web.ignoring()
-                .requestMatchers("/static/**");
-    }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests(authorize -> authorize // 인증, 인가 설정
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/static/**").permitAll() // static 리소스 접근 허용
                         .requestMatchers("/login", "/signup", "/user").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(form -> form // 폼 기반 로그인 설정
+                .formLogin(form -> form
                         .loginPage("/login")
                         .defaultSuccessUrl("/articles")
                 )
-                .logout(logout -> logout // 로그아웃 설정
+                .logout(logout -> logout
                         .logoutSuccessUrl("/login")
                         .invalidateHttpSession(true)
                 )
-                .csrf(csrf -> csrf.disable()); // CSRF 비활성화
+                .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
 
-
-    // 인증 관리자 관련 설정
     @Bean
     public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder, MemberService memberService, MemberDetailService memberDetailService)
             throws Exception {
@@ -61,12 +51,8 @@ public class WebSecurityConfig {
         return authManagerBuilder.build();
     }
 
-    // 패스워드 인코더로 사용할 빈 등록
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
     }
-
-
-
 }
