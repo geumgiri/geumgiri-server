d1419d60a58132aa6842f65bff2e7349fe4f3685
Yangdaehan <yangdh08@naver.com>
2024-09-26 14:10:43 +0900

    fix: #21 - 대출 상환 과정에서 0인 할부 횟수에 대한 에러처리 구현

--- src/main/java/com/tta/geumgiri/loan/application/scheduler/LoanRepaymentScheduler.java
+++ src/main/java/com/tta/geumgiri/loan/application/scheduler/LoanRepaymentScheduler.java
@@ -1,5 +1,6 @@
 package com.tta.geumgiri.loan.application.scheduler;
 
+import com.tta.geumgiri.loan.application.LoanService;
 import com.tta.geumgiri.loan.domain.Loan;
 import com.tta.geumgiri.loan.persistence.LoanRepository;
 import com.tta.geumgiri.account.domain.Account;
@@ -11,31 +12,39 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.util.List;
 
+
+// 스케줄러를 사용해서 자동으로
 @Component
 public class LoanRepaymentScheduler {
 
     private final LoanRepository loanRepository;
+    private final LoanService loanService;
 
     @Autowired
-    public LoanRepaymentScheduler(LoanRepository loanRepository, AccountRepository accountRepository) {
+    public LoanRepaymentScheduler(LoanRepository loanRepository, LoanService loanService) {
         this.loanRepository = loanRepository;
+        this.loanService = loanService;
     }
 
-    @Scheduled(cron = "0 0 0/1 * * ?") // 1시간마다 이벤트 발생
+    @Scheduled(cron = "0 0/1 * * * ?") // 3분마다 실행
     public void processLoanRepayment() {
         LocalDate today = LocalDate.now();
         List<Loan> loans = loanRepository.findByRepaymentDate(today);
 
         for (Loan loan : loans) {
             if (!loan.isPaid()) {
-                // 대출 상환 처리
-                Account account = loan.getAccount();
-
-                // 대출 금액만큼 차감
+                if (loan.getInstallments() <= 0){
+                    System.out.println("상환 횟수가 0인 대출은 처리할 수 없습니다.");
+                    continue;
+                }
                 try {
-                    account.deductBalance(loan.getAmount());
-                    loan.markAsPaid(); // 대출 상환 완료 처리
-                    loanRepository.save(loan);
+                    loanService.payInstallment(loan);
+
+                    if (loan.isPaid()) {
+                        System.out.println("대출 상환 완료: " + loan.getPaidInstallments() + "/" + loan.getInstallments() + "회 상환 완료되었습니다.");
+                    } else {
+                        System.out.println("대출 일부 상환: " + loan.getPaidInstallments() + "/" + loan.getInstallments() + "회차가 상환되었습니다.");
+                    }
                 } catch (IllegalArgumentException e) {
                     System.out.println("잔액 부족으로 대출 상환 실패: " + e.getMessage());
                 }
