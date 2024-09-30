0a6a12418d7f9d1f0018f0d18658730730897c98
Yangdaehan <yangdh08@naver.com>
2024-09-30 17:46:01 +0900

    fix: #23 - 대출 스케줄러 출력 창 개선
    - 이벤트 발생 시 적금 스케줄러와 헷갈리지 않게 개선

--- src/main/java/com/tta/geumgiri/loan/application/scheduler/LoanRepaymentScheduler.java
+++ src/main/java/com/tta/geumgiri/loan/application/scheduler/LoanRepaymentScheduler.java
@@ -40,19 +40,19 @@ public class LoanRepaymentScheduler {
         for (Loan loan : loans) {
             if (!loan.isPaid()) {
                 if (loan.getInstallments() <= 0){
-                    System.out.println("상환 횟수가 0인 대출은 처리할 수 없습니다.");
+                    System.out.println("[대출] 상환 횟수가 0인 대출은 처리할 수 없습니다.");
                     continue;
                 }
                 try {
                     loanService.payInstallment(loan);
 
                     if (loan.isPaid()) {
-                        messageBuilder.append("사용자 ").append(loan.getMember().getName()).append("이(가) 대출한 대출(").append(loan.getId()).append(")의 상환이 완료되었습니다: ").append(loan.getPaidInstallments()).append("/").append(loan.getInstallments()).append("회 상환 완료되었습니다.\n");
+                        messageBuilder.append("[대출] 사용자 ").append(loan.getMember().getName()).append("이(가) 대출한 대출(").append(loan.getId()).append(")의 상환이 완료되었습니다: ").append(loan.getPaidInstallments()).append("/").append(loan.getInstallments()).append("회 상환 완료되었습니다.\n");
                     } else {
-                        messageBuilder.append("사용자 ").append(loan.getMember().getName()).append("이(가) 대출한 대출(").append(loan.getId()).append(")의 일부가 상환되었습니다: ").append(loan.getPaidInstallments()).append("/").append(loan.getInstallments()).append("회차가 상환되었습니다.\n");
+                        messageBuilder.append("[대출] 사용자 ").append(loan.getMember().getName()).append("이(가) 대출한 대출(").append(loan.getId()).append(")의 일부가 상환되었습니다: ").append(loan.getPaidInstallments()).append("/").append(loan.getInstallments()).append("회차가 상환되었습니다.\n");
                     }
                 } catch (IllegalArgumentException e) {
-                    System.out.println("잔액 부족으로 대출 상환 실패: " + e.getMessage());
+                    System.out.println("[대출] 잔액 부족으로 대출 상환 실패: " + e.getMessage());
                 }
             }
 
