4db35acc90cc9741398365b19e7c1b0fa4980b10
Minseo Kim <minseo770@gmail.com>
2024-10-16 16:27:29 +0900

    fix: #4 - myCard, Card 간 연관 관계 수정
    Card에서 myCard가 필요하지 않으므로 단방향 관계로 수정

--- src/main/java/com/tta/geumgiri/card/domain/MyCard.java
+++ src/main/java/com/tta/geumgiri/card/domain/MyCard.java
@@ -23,7 +23,7 @@ public class MyCard {
     @JoinColumn(name = "card_id", nullable = false)
     private Card card;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "account_id", nullable = false)
     private Account account;
 
