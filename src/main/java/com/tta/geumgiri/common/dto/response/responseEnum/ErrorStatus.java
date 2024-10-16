3ef91f81e130cb901ff5c4e21caf850e56ee6d67
Minseo Kim <minseo770@gmail.com>
2024-10-16 16:51:48 +0900

    fix: #4 - 오류 수정

--- src/main/java/com/tta/geumgiri/common/dto/response/responseEnum/ErrorStatus.java
+++ src/main/java/com/tta/geumgiri/common/dto/response/responseEnum/ErrorStatus.java
@@ -27,14 +27,13 @@ public enum ErrorStatus {
    * 회원가입 관련 오류
    */
   DUPLICATE_USER_ID(HttpStatus.CONFLICT.value(), "이미 존재하는 아이디입니다."),
-  DUPLICATE_ACCOUNT_ID(HttpStatus.CONFLICT.value(), "이미 사용중인 계좌입니다.")
+  DUPLICATE_ACCOUNT_ID(HttpStatus.CONFLICT.value(), "이미 사용중인 계좌입니다."),
 
   /**
    *  대출 관련 오류
    */
   LOAN_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "대출이 존재하지 않습니다."),
   INSUFFICIENT_CREDIT(HttpStatus.BAD_REQUEST.value(), "신용등급이 낮아 대출이 불가능합니다."),
-  ACCOUNT_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "Account not found"),
   INVALID_ACCOUNT_OWNER(HttpStatus.BAD_REQUEST.value(), "이 계좌는 해당 회원의 것이 아닙니다."),
   INVALID_REPAYMENT_DATE(HttpStatus.BAD_REQUEST.value(), "상환 날짜는 오늘보다 과거일 수 없습니다."),
   INVALID_INSTALLMENT(HttpStatus.BAD_REQUEST.value(), "상환 횟수는 1 이상이어야 합니다."),
